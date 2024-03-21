2bded99919123ab3d8ca9ebf3886da74ea1ec8ff
Topaz712 <tzam712@gmail.com>
2024-03-20 20:35:43 -0500

    ADD: documented index endpoint

---
+++ spec/integration/users_spec.rb
@@ -0,0 +1,28 @@
+require 'swagger_helper'
+
+RSpec.describe 'users', type: :request do
+
+  path '/users' do
+
+    get 'Retrieves all users' do
+      tags 'Users'
+      produces 'application/json'
+
+      response '200', 'successful' do
+        schema type: :array,
+          items: {
+            type: :object,
+            properties: {
+              id: { type: :integer },
+              name: { type: :string },
+              email: { type: :string }
+            },
+            required: ['id', 'name', 'email']
+          }
+
+        run_test!
+      end
+    end
+    
+  end
+end
